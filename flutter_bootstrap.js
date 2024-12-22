30292133169be24167f55143a816ccf2e944188f
Tapoban Ray <raytapoban@gmail.com>
2024-12-22 19:31:39 +0530

    Fixed a lot of responsiveness iss

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4212870354"
+    serviceWorkerVersion: "1358141028"
   }
 });
