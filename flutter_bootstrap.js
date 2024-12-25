22373bdc647c2d5ca5280b1a8179aae5f4c4ac17
Tapoban Ray <raytapoban@gmail.com>
2024-12-25 15:31:33 +0530

    Fixed multiple issues of MobileLayout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3714048819"
+    serviceWorkerVersion: "1925724459"
   }
 });
