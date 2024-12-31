5a894987c5e15c64549a07b7be9589c34739bbc5
Tapoban Ray <raytapoban@gmail.com>
2024-12-31 17:45:04 +0530

    Corrected TextInputTypes of keyboards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2379274738"
+    serviceWorkerVersion: "3582055626"
   }
 });
